fcd7518faad309c2f4b4045cf46ab91e9946eff0
CAlexByuI <ale20002@byui.edu>
2025-01-16 09:05:56 -0700

    A number guessing game that keeps looping as long as the user chooses to do so

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("Please pick a number: ");
+        // magicNumber = int.Parse(Console.ReadLine());
+        string keepPlaying = "yes";
+        while (keepPlaying == "yes")
+        {    
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 51);
+
+        int guess = -1;
+        
+            while (guess != magicNumber)
+            {
+                Console.Write("Guess the magic number:");
+                guess = int.Parse(Console.ReadLine());
+            
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Congrats, you guessed the number!");
+                }
+            }
+            Console.Write("would you like to keep playing? Type yes or no: ");
+            keepPlaying = Console.ReadLine();
+        }    
     }
 }
