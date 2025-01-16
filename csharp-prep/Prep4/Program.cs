70548b1de0c81ab0f89aa9ffd723fc4a90f7c10d
CAlexByuI <ale20002@byui.edu>
2025-01-16 09:27:09 -0700

    a program that puts user inputted numbers into a list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = 0;
+        do{
+        Console.WriteLine("Please enter a list of numbers, type 0 when finished: ");
+        number = int.Parse(Console.ReadLine());
+        numbers.Add(number);
+    
+        } while (number != 0);
+
+        Console.WriteLine("The sum ofthe numbers is " + numbers.Sum());
+        Console.WriteLine("The Average of all the numbers is " + numbers.Average());
+        Console.WriteLine("The largest number in the list is " + numbers.Max());
+        numbers.Sort();
+        Console.WriteLine("The list in sorted order is: " + string.Join(", ", numbers));
     }
 }
